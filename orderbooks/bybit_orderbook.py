fe9233ee7f19384830eaf57b0874c70a8c5535f3
Mischa Sarac <mischasarac@gmail.com>
2025-06-10 07:46:08 +0930

    Dear lord, please no merge conflicts

--- orderbooks/bybit_orderbook.py
+++ orderbooks/bybit_orderbook.py
@@ -1,40 +1,33 @@
 import requests
 import numpy
 import os
+import ccxt
 
-def get_all_symbols():
-    url = "https://api.bybit.com/v5/market/instruments-info"
-    params = {
-        "category": "linear"
-    }
+# def get_all_symbols():
+#     url = "https://api.bybit.com/v5/market/instruments-info"
+#     params = {
+#         "category": "linear"
+#     }
 
-    try:
-        response = requests.get(url, params=params)
-        response.raise_for_status()
-        data = response.json()
-        for entry in data["result"]["list"]:
-            print(entry["symbol"])
-            if "LAUNCHCOIN" in entry["symbol"]:
-                print("Found:", entry["symbol"])
-    except requests.RequestException as e:
-        print(f"Error: {e}")
+#     try:
+#         response = requests.get(url, params=params)
+#         response.raise_for_status()
+#         data = response.json()
+#         for entry in data["result"]["list"]:
+#             print(entry["symbol"])
+#             if "LAUNCHCOIN" in entry["symbol"]:
+#                 print("Found:", entry["symbol"])
+#     except requests.RequestException as e:
+#         print(f"Error: {e}")
 
-get_all_symbols()
+# get_all_symbols()
 
 
 def get_orderbook(ticker: str):
-    url = "https://api.bybit.com/v5/market/orderbook"
-    params = {
-        "category": "linear",
-        "symbol": ticker,
-        "limit": 2
-    }
+    curr = ccxt.bybit()
+    curr_ticker = f"{ticker}USDT"
+    ob = curr.fetch_order_book(curr_ticker)
 
-    try:
-        response = requests.get(url, params=params)
-        response.raise_for_status()
-        print(response.json())
-    except requests.RequestException as e:
-        print(f"Error: {e}")
+    print(ob)
 
-get_orderbook("LAUNCHCOINUSDT")
+get_orderbook("SKATE")
