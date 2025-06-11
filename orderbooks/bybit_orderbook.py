e5a2b0f5348ec844d51617bd626f2aca997b5053
Mischa Sarac <mischasarac@gmail.com>
2025-06-11 11:43:40 +0930

    Getting ready to merge

--- orderbooks/bybit_orderbook.py
+++ orderbooks/bybit_orderbook.py
@@ -3,25 +3,6 @@ import numpy
 import os
 import ccxt
 
-# def get_all_symbols():
-#     url = "https://api.bybit.com/v5/market/instruments-info"
-#     params = {
-#         "category": "linear"
-#     }
-
-#     try:
-#         response = requests.get(url, params=params)
-#         response.raise_for_status()
-#         data = response.json()
-#         for entry in data["result"]["list"]:
-#             print(entry["symbol"])
-#             if "LAUNCHCOIN" in entry["symbol"]:
-#                 print("Found:", entry["symbol"])
-#     except requests.RequestException as e:
-#         print(f"Error: {e}")
-
-# get_all_symbols()
-
 
 def get_orderbook(ticker: str):
     curr = ccxt.bybit()
